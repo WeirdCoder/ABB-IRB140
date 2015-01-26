f8f550895bc6a8ead91afe7f30002d4b04a2ca69
alexc89 <alexc89@mit.edu>
2015-01-26 12:15:58 -0500

    convertor full robotState

--- robotsuite-lcm-util/IRB140DRCLCM_convertor.py
+++ robotsuite-lcm-util/IRB140DRCLCM_convertor.py
@@ -20,16 +20,43 @@ class abbIRB140DRCLCMConvertor:
         ### Msg Conversion
 
         msgOut.utime = msgIn.timestamp
-        #msgOut.position_3d_t TODO
-        #msgOut.twist_t TODO
+        msgOut.pose = position_3d_t()
+        msgOut.pose.translation = vector_3d_t()
+        msgOut.pose.translation.x = 0.0
+        msgOut.pose.translation.y = 0.0
+        msgOut.pose.trnalsation.z = 0.0
+        msgOut.pose.rotation = quaternion_t()
+        msgOut.pose.rotation.w = 0.0
+        msgOut.pose.rotation.x = 0.0
+        msgOut.pose.rotation.y = 0.0
+        msgOut.pose.rotation.z = 0.0
+        msgOut.twist = twist_t()
+        msgOut.twist.linear_velocity = vector_3d_t()
+        msgOut.twist.linear_velocity.x = 0.0
+        msgOut.twist.linear_velocity.y = 0.0
+        msgOut.twist.linear_velocity.z = 0.0
+        msgOut.twist.angular_velocity = vector_3d_t()
+        msgOut.twist.angular_velocity.x = 0.0
+        msgOut.twist.angular_velocity.y = 0.0
+        msgOut.twist.angular_velocity.z = 0.0
 
         msgOut.num_joints = len(msgIn.joints.pos)
         msgOut.joint_name = ["Joint" + str(i) for i in range(len(msgOut.num_joints))]
         msgOut.joint_position = msgIn.joints.pos
         msgOut.joint_velocity = msgIn.joints.vel
-        #msgOut.joint_effort TODO
-
-        #msgOut.force_torque TODO
+        msgOut.joint_effort = [0.0 for i in range(len(msgOut.num_joints))]
+        
+        msgOut.force_torque = force_torque_t()
+        msgOut.force_torque.l_foot_force_z = 0
+        msgOut.force_torque.l_foot_torque_x = 0
+        msgOut.force_torque.l_foot_torque_y = 0
+        msgOut.force_torque.r_foot_force_z = 0
+        msgOut.force_torque.r_foot_torque_x = 0
+        msgout.force_torque.r_foot_torque_y = 0
+        msgOut.force_torque.l_hand_force = [0,0,0]
+        msgOut.force_torque.l_hand_torque = [0,0,0]
+        msgOut.force_torque.r_hand_force = [0,0,0]
+        msgOut.force_torque.r_hand_torque = [0,0,0]
  
         #Msg Publish
         self.lc.publish("S_ROBOT_STATE", msg.encode())
