728941f3e3aa363c22aeb786d79f4e4557ca8736
hongkai <daihongkai@gmail.com>
2015-01-27 16:42:26 -0500

    add a new lcm types for joint plan

--- robotsuite-lcm-util/IRB140LCM_monitor.py
+++ robotsuite-lcm-util/IRB140LCM_monitor.py
@@ -17,12 +17,12 @@ from lcmtypes import *
 #Message Conversion
 def convertABBstate(joint_pos,joint_vel,cartesian):
     msg = abb_irb140state()
-    msg.timestamp =  time.time()
+    msg.utime=  time.time()*1000000
    
     msg.joints = abb_irb140joints()
     msg.cartesian = abb_irb140cartesian()
-    msg.joints.timestamp = msg.timestamp
-    msg.cartesian.timestamp = msg.timestamp
+    msg.joints.utime = msg.utime
+    msg.cartesian.utime= msg.utime
     msg.joints.pos = joint_pos
     msg.joints.vel = joint_vel
     msg.cartesian.pos = cartesian[0]
@@ -36,8 +36,18 @@ class abbIRB140LCMWrapper:
     
     def __init__(self):
         self.robot = abb.Robot(); #Robot Connection to openABB, input Robot's IP if needed.
-        self.lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
-        self.subscription = self.lc.subscribe("IRB140Input",self.command_handler)
+        self.joint_cmd_lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
+        self.joint_cmd_subscription = self.joint_cmd_lc.subscribe("IRB140Input",self.command_handler)
+        self.joint_plan_lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
+        self.joint_plan_subscription = self.joint_plan_lc.subscribe("IRB140JOINTPLAN",self.plan_handler)
+        
+
+    def plan_handler(self,channel,data):
+        msg = abb_irb140joint_plan.decode(data)
+        for i in range(msg.n_cmd_times):
+            self.robot.addJointPosBuffer(msg.joint_cmd[i].joints)
+        self.robot.executeJointPosBuffer()
+        self.robot.clearJointPosBuffer()
         
     def command_handler(self,channel,data):
         msg = abb_irb140joints.decode(data)
@@ -49,7 +59,7 @@ class abbIRB140LCMWrapper:
 	cartesian = self.robot.getCartesian()
         #ABB drive to LCM conversion
 	msg = convertABBstate(jointPos,[0,0,0,0,0,0],cartesian)
-        self.lc.publish("IRB140STATE", msg.encode())
+        self.joint_cmd_lc.publish("IRB140STATE", msg.encode())
 
     def mainLoop(self,freq):
         pauseDelay = 1.0/freq #In Seconds.
@@ -64,7 +74,7 @@ class abbIRB140LCMWrapper:
             t.start()
             while True:
                 time.sleep(pauseDelay)
-                self.lc.handle()
+                self.joint_cmd_lc.handle()
         except KeyboardInterrupt:
             pass
 
--- robotsuite-lcm-util/lcmtypes/__init__.py
+++
@@ -1,4 +0,0 @@
-from abblcm import abb_irb140state
-from abblcm import abb_irb140joints
-from abblcm import abb_irb140cartesian
-from drc import robot_state_t
